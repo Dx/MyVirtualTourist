84c4012f9d7b8508fe99d24d6c8f25840c85190d
David Cruz <dequis@gmail.com>
2015-10-07 11:34:53 -0500

    Agregado el modelo

--- MyVirtualTourist/ViewController.swift
+++ MyVirtualTourist/ViewController.swift
@@ -7,19 +7,36 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var touristMap: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let longPressRecogniser = UILongPressGestureRecognizer(target: self, action: "createPin:")
+        
+        longPressRecogniser.minimumPressDuration = 1.0
+        touristMap.addGestureRecognizer(longPressRecogniser)
+    }
+    
+    func createPin(gestureRecognizer : UIGestureRecognizer){
+        if gestureRecognizer.state != .Began { return }
+        
+        let touchPoint = gestureRecognizer.locationInView(self.touristMap)
+        let touchMapCoordinate = touristMap.convertPoint(touchPoint, toCoordinateFromView: touristMap)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = touchMapCoordinate
+        
+        touristMap.addAnnotation(annotation)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 

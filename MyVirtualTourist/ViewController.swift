c5925dd7f605dc2864f675dd2e87f4b9622f71b1
David Cruz <dequis@gmail.com>
2015-10-06 23:40:04 -0500

    Initial Commit

---
+++ MyVirtualTourist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyVirtualTourist
+//
+//  Created by Dx on 06/10/15.
+//  Copyright © 2015 Palmera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
